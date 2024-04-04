fc2560d60281bc9cffd961b334485ced51dc7102
BuildTools <unconfigured@null.spigotmc.org>
2024-04-04 22:14:57 +0700

    chui

--- server/supabase.ts
+++ server/supabase.ts
@@ -1,10 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY || !process.env.JWT_SECRET) {
-    throw new Error('Missing environment variable(s)');
-}
+// if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY || !process.env.JWT_SECRET) {
+//     throw new Error('Missing environment variable(s)');
+// }
+
+const SUPABASE_URL = "https://drnjbcaxcnmzhcmyszbx.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRybmpiY2F4Y25temhjbXlzemJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NDI5NDYsImV4cCI6MjAyNjQxODk0Nn0.2gLoBrYGf35oxbKZwkep1AtdkYx1nIYaGjLEfSNUlXU";
 
 export const supabase = createClient(
-    process.env.SUPABASE_URL, 
-    process.env.SUPABASE_KEY
+    SUPABASE_URL,//process.env.SUPABASE_URL, 
+    SUPABASE_KEY//process.env.SUPABASE_KEY
 );
